27da13405be9ea3439e6dfe356cc3e96a655f329
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-09-12 19:04:32 -0700

    changing IBuilder to IApplicationBuilder

--- mvc/Startup.cs
+++ mvc/Startup.cs
@@ -6,7 +6,7 @@ namespace KWebStartup
 {
     public class Startup
     {
-        public void Configure(IBuilder app)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseStaticFiles();
             app.UseErrorPage();
